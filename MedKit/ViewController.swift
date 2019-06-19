660be28b2b75f5dfaf77bf39a8df6646b9dcb413
BS283 <bs283@users-MacBook-Pro.local>
2019-06-19 23:53:14 +0600

    Initial Commit

---
+++ MedKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MedKit
+//
+//  Created by Sium_MBSTU on 19/6/19.
+//  Copyright © 2019 MedKit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
